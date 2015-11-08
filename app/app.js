71c882c858d9c5cd3c749204c9681fbd523c8434
Benjamin Rabin <benjaminrabin@gmail.com>
2015-11-08 01:22:01 -0500

    added stuff

--- app/app.js
+++ app/app.js
@@ -12,12 +12,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 //added express-responsive
 var responsive = require('express-responsive');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -32,6 +31,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//device capture and device routing
+app.use(responsive.deviceCapture());
 
 app.use('/', routes);
 app.use('/users', users);
